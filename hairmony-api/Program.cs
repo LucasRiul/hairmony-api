b40c1ef17011a10a2c9297699160f70adf907562
lucas.riul <lucas.riul@treeinova.com.br>
2025-05-20 17:19:58 -0300

    update cors

--- hairmony-api/Program.cs
+++ hairmony-api/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
         policy => policy
-            .WithOrigins("http://localhost:4230") // ou "*" se for para testes
+            .WithOrigins("http://localhost:4230", "https://hairmony.hair/") 
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
