beebc83c742b3cb9d0c0d8a70dbbe02407716c3a
Lucas Riul Martins <lucasriul85@icloud.com>
2025-05-21 02:18:20 -0300

    cors

--- hairmony-api/Program.cs
+++ hairmony-api/Program.cs
@@ -59,7 +59,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
         policy => policy
-            .WithOrigins("http://localhost:4230", "https://hairmony.hair/") 
+            .WithOrigins("http://localhost:4230", "https://hairmony.hair/", "https://hairmony-api-production.up.railway.app/") 
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
