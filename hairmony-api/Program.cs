2bad80d4ab276474f42d1a4f1a2a95fb5aab177a
Lucas Riul Martins <lucasriul85@icloud.com>
2025-05-20 23:17:01 -0300

    program jwt

--- hairmony-api/Program.cs
+++ hairmony-api/Program.cs
@@ -26,8 +26,8 @@ builder.Services.AddScoped<ILoginService, LoginService>();
 
 #region jwt
 var jwtSettings = builder.Configuration.GetSection("Jwt");
-var secretKey = Encoding.ASCII.GetBytes(jwtSettings["SecretKey"]); //dev
-//var secretKey = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET") ?? throw new Exception("Chave secreta JWT não encontrada."));
+//var secretKey = Encoding.ASCII.GetBytes(jwtSettings["SecretKey"]); //dev
+var secretKey = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET") ?? jwtSettings["SecretKey"] ?? throw new Exception("Chave secreta JWT não encontrada."));
 
 builder.Services.AddAuthentication(options =>
 {
